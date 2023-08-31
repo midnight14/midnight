291a96dd974962cabcb705c90005516a66968969
박성준 <94158097+myrhymetree@users.noreply.github.com>
2023-08-31 16:13:49 +0900

    Revert "유실된 파일복구"
    This reverts commit 90c51b11183d2bec6de5c63b0b0787f54e35fdaf.

--- src/main/java/com/midnight/reservation/repository/ReservationRepository.java
+++ src/main/java/com/midnight/reservation/repository/ReservationRepository.java
@@ -7,5 +7,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
 
-    Reservation findByReservationNo(Integer reservationNo);
+
+
+
+
+
+
 }
